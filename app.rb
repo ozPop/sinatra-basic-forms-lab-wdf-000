5e4b746fa922872d8bdfe847f1d728e91d582f56
ozPop <ozziepopovas@gmail.com>
2016-08-07 15:10:55 -0400

    add puppy class, update controller actions

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    # renders the form
+    erb :create_puppy
+  end
+
+  post '/list_puppy' do
+    #displays puppy
+    @list_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
 
 end
